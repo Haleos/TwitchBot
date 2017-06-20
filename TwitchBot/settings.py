9eaa0cba1963bdb45d84c5cd86a3dc5e88f5dede
Haleos <hero.of.proportion@gmail.com>
2017-06-20 19:38:38 -0400

    Update settings.py

--- TwitchBot/settings.py
+++ TwitchBot/settings.py
@@ -1,5 +1,5 @@
 HOST = "irc.twitch.tv"
 PORT = 6667
-PASS = "oauth:vm0juycqvhhg5p4qrdu0miompny67e"
-IDENT = "h0llow_bot"
-CHANNEL = "h0llowist"
+PASS = "In real file"
+IDENT = "In real file"
+CHANNEL = "In real file"
